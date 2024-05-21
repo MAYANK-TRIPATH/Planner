5ab6e69c7b38da2be7d18310d999902e804be528
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2024-05-21 20:53:25 +0530

    fixes

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../Components/Navbar';
 
 const Notes = () => {
   return (
-    <div>Notes</div>
+    <div className="bg-gray-900 text-white min-h-screen p-10">
+      <Navbar />
+      <h1 className="text-4xl font-bold text-center mb-8 p-16 underline">
+        On the Way...</h1>
+    </div>
   )
 }
 
--- src/pages/Pdf_Notes.jsx
+++ src/pages/Pdf_Notes.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../Components/Navbar';
 
 const Pdf_Notes = () => {
   return (
-    <div>Pdf_Notes</div>
+    
+      <div className="bg-gray-900 text-white min-h-screen p-10">
+      <Navbar />
+      <h1 className="text-4xl font-bold text-center mb-8 p-16 underline">
+        Coming Soon....</h1>
+    </div>
   )
 }
 
